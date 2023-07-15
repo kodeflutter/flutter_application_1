688dcfe6e28f503b7bd966d9526f1a10157855cf
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-15 12:23:28 +0530

    b3

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  final text =
+      '''Dart is a programming language designed by Lars Bak and Kasper Lund and developed by Google. The programming language can be used to develop web and mobile apps as well as server and desktop applications. It is an object-oriented, class-based, garbage-collected language with C-style syntax. Wikipedia''';
   runApp(Text(
-    "Hello World",
+    text.trimLeft(),
     textDirection: TextDirection.ltr,
+    style: TextStyle(
+      color: Colors.grey,
+      fontSize: 32,
+      fontWeight: FontWeight.bold,
+    ),
   ));
 }
