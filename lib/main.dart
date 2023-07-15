2436f4dadfc09d71dccfb8a57ad4702755402479
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-15 12:15:09 +0530

    b2 simplest widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Text("Hello World"),
-      ),
-    );
-  }
+  runApp(Text(
+    "Hello World",
+    textDirection: TextDirection.ltr,
+  ));
 }
