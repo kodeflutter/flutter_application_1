324391969736cf717d593fc6ca1eac1bc29c648e
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-15 12:40:12 +0530

    b6 simple container

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  const text = '''Dart is a programming ''';
   runApp(
     Container(
-      child: Text(
-        text,
-        textDirection: TextDirection.ltr,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 32,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      alignment: Alignment.center,
       decoration: BoxDecoration(color: Colors.green),
     ),
   );
