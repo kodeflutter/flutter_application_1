b70a31e698410724ca8361b860ebec658b3f3f3c
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-15 16:23:57 +0530

    column with container.

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Container(
-      decoration: BoxDecoration(color: Colors.green),
+    Column(
+      children: [
+        SampleContainer(),
+        SampleContainer(),
+      ],
     ),
   );
+}
+
+class SampleContainer extends StatelessWidget {
+  const SampleContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.only(bottom: 2),
+      decoration: BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        "Helllo Worlddd",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
 }
